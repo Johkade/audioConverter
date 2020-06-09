0e9aa8b43d887980201d7a66ca0f1754c1f4660b
johkade <j.kadeii@gmx.de>
2020-06-09 15:53:18 +0200

    working example for conversion

--- convert.sh
+++ convert.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #convert mp3 to ogg
 
 #ffmpeg -i input.mp3 -c:a libvorbis -q:a 4 output.ogg
@@ -5,17 +6,13 @@
 
 for f in ./audiosToConvert/*.mp3
  do 
- fullname="$f"
-echo "$fullname"
- filename = "$(basename -- $fullname)"
-echo "$filename" 
+	echo "fullname is: $f"
+ 	filename=$(basename -- $f)
+	echo "filename is: $filename"  
  
- 
- 
- 
-ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%mp3/ogg}"; done
+	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%mp3/ogg}"
 
-
+ done
 
 
 
