ea3588238c353fbcda23966cd039898fe8d0f543
johannes.kade <johannes.kade@smartlytic.de>
2020-06-09 15:17:01 +0200

    test filename formats

--- convert.sh
+++ convert.sh
@@ -3,4 +3,14 @@
 #ffmpeg -i input.mp3 -c:a libvorbis -q:a 4 output.ogg
 
 
-for f in ./audiosToConvert/*.mp3; do ffmpeg -i "$f" -c:a libvorbis -q:a 4 "converted/${f/%mp3/ogg}"; done
+for f in ./audiosToConvert/*.mp3
+ do 
+ fullname = $f
+echo $fullname
+ filename = "$(basename -- $fullname)"
+echo $filename 
+ 
+ 
+ 
+ 
+# ffmpeg -i "$f" -c:a libvorbis -q:a 4 "${f/%mp3/ogg}"; done
