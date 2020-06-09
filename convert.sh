a053bad98a9ed43c585093848c0d7e0d4fad4c41
johkade <j.kadeii@gmx.de>
2020-06-09 16:04:10 +0200

    first conversion

--- convert.sh
+++ convert.sh
@@ -10,10 +10,17 @@ for f in ./audiosToConvert/*.mp3
  	filename=$(basename -- $f)
 	echo "filename is: $filename"  
  
-	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%mp3/ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.mp3/q01.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.mp3/q02.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.mp3/q03.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.mp3/q04.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.mp3/q05.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.mp3/q06.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.mp3/q07.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.mp3/q08.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.mp3/q09.ogg}"
+	ffmpeg -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.mp3/q10.ogg}"
+
+
 
  done
-
-
-
-
