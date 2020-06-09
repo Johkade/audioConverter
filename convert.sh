9345b51aff6d13227ab452f5b5a5a109c9d839c2
johannes.kade <johannes.kade@smartlytic.de>
2020-06-09 16:29:00 +0200

    added wav conversion

--- convert.sh
+++ convert.sh
@@ -6,21 +6,31 @@
 
 for f in ./audiosToConvert/*.mp3
  do 
-	echo "fullname is: $f"
- 	filename=$(basename -- $f)
-	echo "filename is: $filename"  
+ 	filename=$(basename -- $f)	echo "filename is: $filename"  
+	ffmpeg -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.mp3/_q080.ogg}" #80
+	ffmpeg -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.mp3/_q096.ogg}" #96
+	ffmpeg -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.mp3/_q112.ogg}" #112
+	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.mp3/_q128.ogg}" #128
+	ffmpeg -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.mp3/_q160.ogg}" #160
+	ffmpeg -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.mp3/_q192.ogg}" #192
+	ffmpeg -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.mp3/_q224.ogg}" #224
+	ffmpeg -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.mp3/_q256.ogg}" #256
+	ffmpeg -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.mp3/_q320.ogg}" #320
+	ffmpeg -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.mp3/_q500.ogg}" #500
+ done
  
-	ffmpeg -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.mp3/q01.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.mp3/q02.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.mp3/q03.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.mp3/q04.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.mp3/q05.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.mp3/q06.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.mp3/q07.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.mp3/q08.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.mp3/q09.ogg}"
-	ffmpeg -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.mp3/q10.ogg}"
-
-
-
+ for f in ./audiosToConvert/*.wav
+ do 
+ 	filename=$(basename -- $f)
+ 
+	ffmpeg -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.wav/_q080.ogg}" #80
+	ffmpeg -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.wav/_q096.ogg}" #96
+	ffmpeg -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.wav/_q112.ogg}" #112
+	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.wav/_q128.ogg}" #128
+	ffmpeg -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.wav/_q160.ogg}" #160
+	ffmpeg -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.wav/_q192.ogg}" #192
+	ffmpeg -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.wav/_q224.ogg}" #224
+	ffmpeg -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.wav/_q256.ogg}" #256
+	ffmpeg -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.wav/_q320.ogg}" #320
+	ffmpeg -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.wav/_q500.ogg}" #500
  done
