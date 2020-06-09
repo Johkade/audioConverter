13e43ea8b98bdbc96c51e863137aa6e3288ce5ee
johannes.kade <johannes.kade@smartlytic.de>
2020-06-09 14:18:58 +0200

    added: new outupt loc

--- convert.sh
+++ convert.sh
@@ -3,4 +3,4 @@
 #ffmpeg -i input.mp3 -c:a libvorbis -q:a 4 output.ogg
 
 
-for f in ./toConvert/*.mp3; do ffmpeg -i "$f" -c:a libvorbis -q:a 4 "${f/%mp3/ogg}"; done
+for f in ./toConvert/*.mp3; do ffmpeg -i "$f" -c:a libvorbis -q:a 4 "converted/${f/%mp3/ogg}"; done
