168051e20fd688d44f873f661fc63e0d1e16cb24
johkade <j.kadeii@gmx.de>
2020-07-25 18:27:52 +0200

    conversion files

--- convert.sh
+++ convert.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 #convert mp3 to ogg
-
-#ffmpeg -i input.mp3 -c:a libvorbis -q:a 4 output.ogg
-
-
-for f in ./audiosToConvert/*.mp3
- do 
- 	filename=$(basename -- $f)	
-
-	echo "f is: " $f "and filename is: " $filename
-	echo
-	echo
-	echo "output location is: " "./converted/${filename/%.mp3/_q080.ogg}"
-	echo
-	echo	
-
-
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.mp3/_q080.ogg}" #80
-#	ffmpeg -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.mp3/_q096.ogg}" #96
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.mp3/_q112.ogg}" #112
-#	ffmpeg -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.mp3/_q128.ogg}" #128
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.mp3/_q160.ogg}" #160
-#	ffmpeg -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.mp3/_q192.ogg}" #192
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.mp3/_q224.ogg}" #224
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.mp3/_q256.ogg}" #256
-#	ffmpeg -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.mp3/_q320.ogg}" #320
-	#ffmpeg -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.mp3/_q500.ogg}" #500
- done
  
  for f in ./audiosToConvert/*.wav
  do 
@@ -38,33 +11,40 @@ for f in ./audiosToConvert/*.mp3
 	echo "$f" is being converted to "./converted/${filename/%.wav/_q080.ogg}" and filename is: "$filename"
 	echo
 
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a -2 "./converted/${filename/%.wav/_q032_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a -1 "./converted/${filename/%.wav/_q048_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 0 "./converted/${filename/%.wav/_q064_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 1 "./converted/${filename/%.wav/_q080_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 2 "./converted/${filename/%.wav/_q096_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 3 "./converted/${filename/%.wav/_q112_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 4 "./converted/${filename/%.wav/_q128_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 5 "./converted/${filename/%.wav/_q160_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 6 "./converted/${filename/%.wav/_q192_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 7 "./converted/${filename/%.wav/_q224_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 8 "./converted/${filename/%.wav/_q256_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 9 "./converted/${filename/%.wav/_q320_mono.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 10 "./converted/${filename/%.wav/_q500_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 9 -ar 44100 -map a "./converted/${filename/%.wav/_q320_44_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 9 -ar 44100 -map a "./converted/${filename/%.wav/_q320_44_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 9 -ar 22050 -map a "./converted/${filename/%.wav/_q320_22_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 9 -ar 22050 -map a "./converted/${filename/%.wav/_q320_22_mono.ogg}"
 
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a -2 "./converted/${filename/%.wav/_q032.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a -1 "./converted/${filename/%.wav/_q048.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 0 "./converted/${filename/%.wav/_q064.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 1 "./converted/${filename/%.wav/_q080.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 2 "./converted/${filename/%.wav/_q096.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 3 "./converted/${filename/%.wav/_q112.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 4 "./converted/${filename/%.wav/_q128.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 5 "./converted/${filename/%.wav/_q160.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 6 "./converted/${filename/%.wav/_q192.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 7 "./converted/${filename/%.wav/_q224.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 8 "./converted/${filename/%.wav/_q256.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 9 "./converted/${filename/%.wav/_q320.ogg}"
-ffmpeg -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 10 "./converted/${filename/%.wav/_q500.ogg}"
+echo "320 done"
+
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 6 -ar 44100 -map a "./converted/${filename/%.wav/_q192_44_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 6 -ar 44100 -map a "./converted/${filename/%.wav/_q192_44_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 6 -ar 22050 -map a "./converted/${filename/%.wav/_q192_22_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 6 -ar 22050 -map a "./converted/${filename/%.wav/_q192_22_mono.ogg}"
+
+echo "192 done"
+
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 4 -ar 44100 -map a "./converted/${filename/%.wav/_q128_44_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 4 -ar 44100 -map a "./converted/${filename/%.wav/_q128_44_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 4 -ar 22050 -map a "./converted/${filename/%.wav/_q129_22_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 4 -ar 22050 -map a "./converted/${filename/%.wav/_q128_22_mono.ogg}"
+
+echo "128 done"
+
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 2 -ar 44100 -map a "./converted/${filename/%.wav/_q096_44_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 2 -ar 44100 -map a "./converted/${filename/%.wav/_q096_44_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 2 -ar 22050 -map a "./converted/${filename/%.wav/_q096_22_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 2 -ar 22050 -map a "./converted/${filename/%.wav/_q096_22_mono.ogg}"
+
+echo "96 done"
+
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 0 -ar 44100 -map a "./converted/${filename/%.wav/_q064_44_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 0 -ar 44100 -map a "./converted/${filename/%.wav/_q064_44_mono.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -q:a 0 -ar 22050 -map a "./converted/${filename/%.wav/_q064_22_stereo.ogg}"
+ffmpeg -y -loglevel warning -nostats -i "$f" -c:a libvorbis -ac 1 -q:a 0 -ar 22050 -map a "./converted/${filename/%.wav/_q064_22_mono.ogg}"
+
+echo "64 done"
 
 
  done
